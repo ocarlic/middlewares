29f2c739cd2ee1ac738f2cfabd05110c1e24bbe9
Victor Carli <victorcarlibr@gmail.com>
2022-07-10 16:21:26 -0300

    check exists user account

--- src/index.js
+++ src/index.js
@@ -10,7 +10,17 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.header
+
+  const userAlreadyExists = users.some(user => user.username === username)
+
+  if(!userAlreadyExists) {
+    return response.status(404).json({ error: 'User not found' })
+  }
+
+  request.user = username
+
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
